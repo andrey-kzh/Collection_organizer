091e9490b2aff074d13415e6c011e75a7090ca3f
Quietmars <akpost@gmail.com>
2022-08-03 17:27:29 +0300

    [fix] change search by categories

--- server/model/search.js
+++ server/model/search.js
@@ -21,32 +21,10 @@ module.exports = {
                 'LEFT JOIN related_category ON (catalog.id = related_category.catalog_id) ' +
                 'LEFT JOIN category ON (category.id = related_category.category_id) ' +
                 'GROUP BY catalog.id ORDER BY catalog.id DESC LIMIT $1 OFFSET $2 ', [limit, offset],
-
             )
             .then((data) => data)
             .catch((error) => {
                 throw error;
-            });
-    },
-
-    findCatalogCount(search, categories) {
-        let connector;
-        let categoriesQuery;
-        (!search) ? connector = 'OR': connector = 'AND';
-        (categories.length > 0) ? categoriesQuery = `${connector} category.id IN (${categories}) `: categoriesQuery = '';
-
-        return db.manyOrNone(
-                'SELECT catalog.id ' +
-                'FROM catalog ' +
-                'LEFT JOIN related_category ON (catalog.id = related_category.catalog_id) ' +
-                'LEFT JOIN category ON (category.id = related_category.category_id) ' +
-                'WHERE catalog.title_idx || catalog.anons_idx @@ plainto_tsquery($1) ' +
-                '$2:raw' +
-                'GROUP BY catalog.id ', [search, categoriesQuery],
-            )
-            .then((data) => data)
-            .catch((error) => {
-                // throw error;
             });
     },
 
@@ -67,7 +45,7 @@ module.exports = {
                 'LEFT JOIN category ON (category.id = related_category.category_id) ' +
                 'WHERE setweight(catalog.title_idx, \'A\') || setweight(catalog.anons_idx, \'B\') @@ plainto_tsquery($1) ' +
                 '$2:raw' +
-                'GROUP BY catalog.id LIMIT $3 OFFSET $4 ', [search, categoriesQuery, limit, offset],
+                'GROUP BY catalog.id LIMIT $3:raw OFFSET $4 ', [search, categoriesQuery, limit, offset],
             )
             .then((data) => data)
             .catch((error) => {
